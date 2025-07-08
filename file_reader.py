5cf9e92c7a74a3376877f768adff03faad0daf6a
10078303 <jeff.suen@ucalgary.ca>
2025-07-07 23:35:52 -0600

    Reader works but unable to find first cell

--- file_reader.py
+++ file_reader.py
@@ -1,8 +1,19 @@
 import pandas as pd
 
-df = pd.read_excel("input.xlsx")
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 1000)
 
-print(df.head())
+df = pd.read_excel("input.xlsx", header=None)
+# df = pd.read_excel("input.xlsx", header=6, usecols="B:K")
+# parse = df.parse()
+
+# print(df.head())
+
+print(df.iloc[:20, :10])
+
+# xlsx format is fucked need to write a function with a for loop to find cell A1
+# Will also need a global var for these
 
 def hello_world():
     print ("Hello World")
