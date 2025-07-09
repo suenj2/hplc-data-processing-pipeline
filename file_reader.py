bf80838d120c7757650e58820984a8ef6222d7da
10078303 <jeff.suen@ucalgary.ca>
2025-07-09 00:06:17 -0600

    Parsing excel successful.

--- file_reader.py
+++ file_reader.py
@@ -4,22 +4,18 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', 1000)
 
-df = pd.read_excel("input.xlsx", header=None)
-# df = pd.read_excel("input.xlsx", header=6, usecols="B:K")
-# parse = df.parse()
+df = pd.read_excel("input.xlsx", sheet_name="PFAS Kitcholm soils 3,4", header=None)
 
-# print(df.head())
+print(df.iloc[0,0])
 
-# print(df.iloc[:20, :10])
+def exp_num():
+    return False
 
-for i in range(10):
-    for j in range(10):
-        val = df.iloc[i, j]
-        if pd.notna(val):
-            print(f"[{i}:{j}] -> {repr(val)}")
+# for i in range(4):
+#     for j in range(4):
+#         val = df.iloc[i, j]
+#         if pd.notna(val):
+#             print(f"[{i}:{j}] -> {repr(val)}")
 
 # xlsx format is fucked need to write a function with a for loop to find cell A1
 # Will also need a global var for these
-
-def hello_world():
-    print ("Hello World")
--- main.py
+++ main.py
@@ -16,7 +16,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    file_reader.hello_world()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
