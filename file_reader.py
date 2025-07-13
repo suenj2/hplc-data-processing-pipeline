97fa455b758542ebddbee09a10b8bf4c878514f5
10078303 <jeff.suen@ucalgary.ca>
2025-07-13 02:59:50 -0600

    Function that returns the cell location of experiment title complete

--- file_reader.py
+++ file_reader.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from cell_coordinate_converter import CellCoordinateConverter
+
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', 1000)
@@ -18,11 +20,13 @@ class FileReader:
             if isinstance(cell, str) and "Compound" in cell:
                 print(cell)
 
-    # def exp_start_cell(self, exp_num):
-    #     for rows in range(self.num_rows):
-    #         cell = self.df.iloc[rows, 0]
-    #         if isinstance(cell, str) and f"Compound {exp_num}" in cell:
-    #             print(f"Found {cell} at ")
+    def exp_start_cell(self, exp_num):
+        for rows in range(self.num_rows):
+            cell = self.df.iloc[rows, 0]
+            if isinstance(cell, str) and f"Compound {exp_num}" in cell:
+                print(f'Found "{cell}" at cell location: {CellCoordinateConverter.to_excel_coord(0, rows)}')
+                return (0, rows)
+        print(f"Experiment number {exp_num} not found")
 
     # def exp_row_range(self):
     #
@@ -35,6 +39,7 @@ class FileReader:
 reader = FileReader("input.xlsx", sheet_name="PFAS Kitcholm soils 3,4")
 FileReader.read_file_meta_data(reader)
 # FileReader.list_exps(reader)
+# FileReader.exp_start_cell(reader, 1)
+FileReader.exp_start_cell(reader, 133)
 
-# Need to converter from int to excel format (0, 0) -> "A1"
 # Will also need a global var for these
