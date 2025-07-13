8107d0f300ce9479e5a363f471229d876e05e05e
10078303 <jeff.suen@ucalgary.ca>
2025-07-13 02:24:44 -0600

    cell_coordinate_converter.py is complete

--- cell_coordinate_converter.py
+++ cell_coordinate_converter.py
@@ -1,4 +1,24 @@
-# class CellCoodinateConverter:
-    # @staticmethod
+class CellCoordinateConverter:
+    @staticmethod
 
-    # def to_excel_coord(row, col):
+    def int_to_excel_col(int):
+        int += 1
+        col_alpha = []
+        while int > 0:
+            int -= 1
+            remainder = int % 26
+            col_alpha.append(chr(ord("A") + remainder))
+            int = int // 26
+        return ''.join(reversed(col_alpha))
+
+    def to_excel_coord(row, col):
+        col_str = CellCoordinateConverter.int_to_excel_col(row)
+        row_str = str(col + 1)
+        return f"{col_str}{row_str}"
+
+# Export the following to a unit test
+# print(CellCoordinateConverter.int_to_excel_col(0))
+# print(CellCoordinateConverter.int_to_excel_col(25))
+# print(CellCoordinateConverter.int_to_excel_col(26))
+#
+# print(CellCoordinateConverter.to_excel_coord(0, 0))  # Should print "A1"
