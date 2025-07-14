9cff03daf1c4eef77fb2ee4a29c4db2dc426dfba
10078303 <jeff.suen@ucalgary.ca>
2025-07-14 14:04:36 -0600

    data_processor.py has an added attribute of the row of the first non-calibration run. Function is tested

--- data_processor.py
+++ data_processor.py
@@ -21,6 +21,21 @@ class DataProcessor:
     def __str__(self):
         return f"DataProcessor holding DataFrame:\n{self.df}"
 
+    def find_first_run(self): #returns the row of the first non-calibration run
+        sample_text_col = 1
+        first_exp_row = 0
+        MeOH_wash_count = 0
+        for row in range(2, self.row_size):
+            if self.df.iloc[row, sample_text_col] == "MeOH":
+                MeOH_wash_count += 1
+            else:
+                MeOH_wash_count = 0
+            if MeOH_wash_count == 2:
+                first_exp_row = row + 1
+                print(f"First experiment was found at row {first_exp_row}")
+                return first_exp_row
+        raise ValueError(f"First run not found")
+
     def pre_format(self):
         for rows in range(self.row_size):
             for cols in range(7, self.col_size):
@@ -34,19 +49,4 @@ class DataProcessor:
             if pd.isna(self.df.iloc[row, 7]):
                 result = float(self.df.iloc[row, 4])/float(self.df.iloc[row, 5]) #Area/IS Area
                 self.df.iloc[row, 7] = result
-
-    def find_first_run(self): #returns the row of the first non-calibration run
-        sample_text_col = 1
-        first_exp_row = 0
-        MeOH_wash_count = 0
-        for row in range(2, self.num_rows):
-            if self.df.iloc[row, sample_text_col] == "MeOH":
-                MeOH_wash_count += 1
-            else:
-                MeOH_wash_count = 0
-            if MeOH_wash_count == 2:
-                first_exp_row = row + 1
-                return first_exp_row
-        raise ValueError(f"First run not found")
-
 
