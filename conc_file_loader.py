e384619b13f5cc2eb460a8ebfc1d32baf7b52ec1
10078303 <jeff.suen@ucalgary.ca>
2025-07-14 18:07:14 -0600

    conc_file_loader.py class added to read starting material concentrations

---
+++ conc_file_loader.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+
+class ConcFileLoader:
+    def __init__(self, file_path):
+        ext = os.path.splitext(file_path)[1].lower()
+
+        if ext == ".csv":
+            self.df = pd.read_csv(file_path, header=None)
+        elif ext == (".xls", ".xlsx"):
+            self.df = pd.read_excel(file_path, sheet_name=None, header=None)
+        else:
+            raise ValueError(f"Unsupported file type: {ext}")
+
+        self.num_rows, self.num_cols = self.df.shape
+
+    def read_file_meta_data(self):
+        print(f".xlsx file contains {self.num_rows} rows and {self.num_cols} columns.")
+
+reader = ConcFileLoader("input/concentration.csv")
+ConcFileLoader.read_file_meta_data(reader)
+
