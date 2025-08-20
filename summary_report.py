daccad1bc0e2f41236f996babe29ad488f96fdf6
Jeff-4770K <jeffreyjunyansuen@hotmail.com>
2025-08-19 21:08:12 -0600

    summary_extraction() - correctly produces a 2D list. Next step is to convert to a pandas dataframe to be written into a summary output spreadsheet

--- summary_report.py
+++ summary_report.py
@@ -8,8 +8,18 @@ class SummaryReport:
     def append_headers(self):
         self.summary_dynamic_list.append(self.sample_list)
 
+    def find_exp_label(self, string):
+        parts = string.split(": ", 1)
+        if len(parts) < 2:
+            raise ValueError(f"Title doesn't contain ': ': {string}")
+        right_side = parts[1]  # everything after the first ": "
+        return right_side
+
     def summary_extraction(self, processed_dataframe):
-        exp_label = processed_dataframe.find_exp_attributes()[1]
+        # exp_label = processed_dataframe.find_exp_attributes()[1]
+        exp_title = str(processed_dataframe.df.iloc[0, 0])
+        exp_label = self.find_exp_label(exp_title)
+        print(exp_label)
         row_extracted_data = [exp_label] #initialize list and append label
         sample_text_col = 1
         calcculated_avg_col = 13
@@ -25,6 +35,7 @@ class SummaryReport:
                     break  # stop scanning rows once match is found
             if not found:
                 row_extracted_data.append(np.nan)  # placeholder to keep dimensions square
-
-        print(row_extracted_data)
+        # print(row_extracted_data)
+        self.summary_dynamic_list.append(row_extracted_data)
+        print(self.summary_dynamic_list)
 
