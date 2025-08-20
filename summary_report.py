4ba5cbe78d5adec19a2228e67ebf1d17aafb13fd
Jeff-4770K <jeffreyjunyansuen@hotmail.com>
2025-08-19 20:09:40 -0600

    summary_extraction() is able to extract the whole row of data. The label is currently not printing a unique label but rather just the compound name. Duplicates will not be registered as unique entries.

--- summary_report.py
+++ summary_report.py
@@ -12,13 +12,31 @@ class SummaryReport:
         #Need for loop to iterate over the titles checking for prefix and suffix
         #When the conditional is met, look to the right and extract summary data
         row_extracted_data = []
+        # sample_text_col = 1
+        # calcculated_avg_col = 13
+        # for analyte in self.sample_list:
+        #     for row in range(processed_dataframe.row_size):
+        #         cell_value = processed_dataframe.df.iloc[row, sample_text_col]
+        #         if isinstance(cell_value, str) and cell_value.startswith(analyte) and cell_value.endswith("A"):
+        #             extracted_average = processed_dataframe.df.iloc[row, calcculated_avg_col]
+        #             print(f"{analyte}: {extracted_average}")
+
+        exp_label = processed_dataframe.find_exp_attributes()[1]
+        row_extracted_data = [exp_label] #initialize list and append label
         sample_text_col = 1
         calcculated_avg_col = 13
-        for analyte in self.sample_list:
-            for row in range(processed_dataframe.row_size):
+
+        for analyte in (self.sample_list[1:]):
+            found = False
+            for row in range(processed_dataframe.row_size): #skip first col
                 cell_value = processed_dataframe.df.iloc[row, sample_text_col]
                 if isinstance(cell_value, str) and cell_value.startswith(analyte) and cell_value.endswith("A"):
                     extracted_average = processed_dataframe.df.iloc[row, calcculated_avg_col]
-                    print(f"{analyte}: {extracted_average}")
+                    row_extracted_data.append(extracted_average)
+                    found = True
+                    break  # stop scanning rows once match is found
+            if not found:
+                row_extracted_data.append(np.nan)  # placeholder to keep dimensions square
 
+        print(row_extracted_data)
 
